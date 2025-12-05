2ac300e70336dbe4676b11cbca76cf63a7a745e3
Shivani0409-git <shivanipanchal0409@gmail.com>
2025-12-05 21:59:44 +0530

    added user egistration controllers

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,9 +1,54 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/apiError.js";
+import {User} from "../models/users.models.js";
+import {ApiResponses} from "../utils/apiResponses.js";
 
 const registerUser = asyncHandler(async (req, res)=>{
-        res.status(200).json({
-                message: "User registered successfully"
+        // res.status(200).json({
+        //         message: "User registered successfully"
+        // })
+        const {fullname, email, username, password} = req.body;
+        console.log("User Data:", {fullname, email, username, password});
+
+        if([fullname, email, username, password].some((fields)=> fields?.trim() === "")){
+                throw new ApiError(400, "All fields are required");
+        }
+
+        User.findOne({
+                $or: [{username},{email}]
         })
+        
+        if(existedUser){
+                throw new ApiError(409, "Username or email already exists");
+        }
+
+        const avatarlocalPath = req.files?.avtar?.[0]?.path;
+        const coverImagePath = req.files?.coverImagePath[0]?.path;
+        if(!avatarlocalPath){
+                throw new ApiError(400, "Avtar image is required");
+        }
+
+        const user = await User.create(
+                {
+                   fullname,
+                   avtar: avatarlocalPath,
+                   coverImage: coverImagePath||"",
+                   email,
+                   username: username.toLowerCase(),
+                   password
+                }
+        )
+
+        const createdUser = await findbyId(user._id).select("-password -refreshTokens");
+
+        if(!createdUser){
+                throw new ApiError(500, "User registration failed");
+        }
+
+        return res.status(201).json(
+                new ApiResponses(200, "User registered successfully", createdUser)
+        )
+
     }
 )
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,20 @@
 import { Router } from "express";
 import { registerUser } from "../controller/user.controller.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avtar",
+            maxCount: 1
+        },
+        {
+            name: "coverPhoto",
+            maxCount: 1
+        }
+    ]),
+    registerUser);
 
 export default router;
